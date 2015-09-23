ec1dd70cc775717c1bb0c7f0133b7b8323440f4e
Euclides Filho <euclides5414@gmail.com>
2015-09-23 09:55:07 +0200

    small fixes

--- ml_utils/__init__.py
+++ ml_utils/__init__.py
@@ -1,2 +1,8 @@
-__author__ = 'euclides'
-from ml_utils import *
+from __future__ import division, print_function
+__author__ = 'Euclides Fernandes Filho <euclides5414@gmail.com>'
+"""
+ml_utils
+Copyright (C) 2015  Euclides Fernandes Filho <euclides5414@gmail.com>
+http://www.gnu.org/licenses/gpl-2.0.html#SEC4
+"""
+from . import *
